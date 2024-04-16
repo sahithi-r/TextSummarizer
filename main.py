1b13865bd99fe96d378f0aad90f4bcaa1270adb2
sahithi-r <30996149+sahithi-r@users.noreply.github.com>
2024-04-16 15:09:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask_ngrok import run_with_ngrok
 from flask import Flask, render_template, request
-
+import ngrok
 from transformers import pipeline
 summarizer = pipeline('summarization')
 
