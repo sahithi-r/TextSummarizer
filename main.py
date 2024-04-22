d9d16ba769f61e9d11c23c2a48f3673f9ba8680a
sahithi-r <30996149+sahithi-r@users.noreply.github.com>
2024-04-21 20:18:05 -0500

    Adding extractive and abstractive routes

--- main.py
+++ main.py
@@ -1,37 +1,60 @@
-from flask_ngrok import run_with_ngrok
-from flask import Flask, render_template, request
 from pyngrok import ngrok
+from flask import Flask, render_template, request, jsonify
 from transformers import pipeline
+import numpy as np
+import networkx as nx
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-summarizer = pipeline('summarization')
+nltk.download('punkt')
 
 # Load model
-pipe = pipeline('summarization')
-#pipe.to("cuda")
+summarizer = pipeline('summarization')
 
 # Start flask app and set to ngrok
 app = Flask(__name__)
-run_with_ngrok(app)
 
+public_url = ngrok.connect()
+print(' * ngrok tunnel:', public_url)
 
 @app.route('/')
 def initial():
     return render_template('index.html')
 
+@app.route('/submit-text-abs', methods=['POST'])
+def generate_summary_abs():
+    prompt = request.form['summarizationInput']
+    print(f"Generating summary of: {prompt}")
 
-@app.route('/submit-text', methods=['POST'])
-def generate_image():
-    prompt = request.form['abstractiveInput']
-    print(f"Generating an image of {prompt}")
-
-    summary = pipe(prompt)
+    summary = summarizer(prompt)
     print("Summary generated!")
 
+    #return jsonify({'abstractiveOutput': summary[0]['summary_text']})
+    return summary[0]['summary_text']
 
-    print("Sending summary ...")
-    return render_template('index.html', abstractiveOutput=summary)
 
+@app.route('/submit-text-ext', methods=['POST'])
+def generate_summary_ext():
+  num_sentences=3
+  text = request.form['summarizationInput']
+
+  sentences = sent_tokenize(text)
+
+  tokenized_sentences = [word_tokenize(sentence.lower()) for sentence in sentences]
+  preprocessed_sentences = [" ".join(words) for words in tokenized_sentences]
+  vectorizer = TfidfVectorizer()
+  tfidf_matrix = vectorizer.fit_transform(preprocessed_sentences)
+  similarity_matrix = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+  scores = np.sum(similarity_matrix, axis=1)
+  ranked_sentences = sorted(((scores[i], i) for i in range(len(sentences))), reverse=True)
+  top_sentences = sorted(ranked_sentences[:num_sentences], key=lambda x: x[1])
+
+  summary = ' '.join([sentences[idx] for score, idx in top_sentences])
+
+  return summary
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(port=80)
